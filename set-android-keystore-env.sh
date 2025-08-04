019d443e9c09df5dd437ba8a778a8bb5d8f2a95a
umairkhalidsidd1 <154114923+umairkhalidsidd1@users.noreply.github.com>
2025-08-04 16:34:52 +0500

    with key of release

---
+++ set-android-keystore-env.sh
@@ -0,0 +1,6 @@
+# Android release keystore environment variables for Prifina
+# Add your actual passwords below
+export MYAPP_UPLOAD_STORE_FILE=android/app/my-upload-key.keystore
+export MYAPP_UPLOAD_STORE_PASSWORD=umairkhalid
+export MYAPP_UPLOAD_KEY_ALIAS=my-key-alias
+export MYAPP_UPLOAD_KEY_PASSWORD=umairkhalid
